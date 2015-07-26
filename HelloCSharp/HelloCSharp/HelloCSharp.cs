6d7313fde32eff87cc4f43f5a251cce2f66995b3
Dessyslava Wasseva <desssssy@gmail.com>
2015-07-26 21:38:42 +0300

    Added 2nd and 3rd line
    Added occupation and name.

--- HelloCSharp/HelloCSharp/HelloCSharp.cs
+++ HelloCSharp/HelloCSharp/HelloCSharp.cs
@@ -4,6 +4,8 @@ class HelloCSharp
 {
 	public static void Main ()
 	{
-		Console.WriteLine ("Hello, C#");
+		Console.WriteLine ("Hello, C#!");
+		Console.WriteLine ("I am a student in Telerik Academy.");
+		Console.WriteLine ("My name is Dessy.");
 	}
 }
