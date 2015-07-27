6d849f23d181ef8bacdf1d65d1ea7c1f061846c2
Dessyslava Wasseva <desssssy@gmail.com>
2015-07-27 20:08:32 +0300

    Whole HomeWork completed
    Added the rest of the tasks. Homework compleated.

---
+++ AgeInTenYears/AgeInTenYears/AgeInTenY.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+	class AgeInTen
+	{
+		public static void Main ()
+		{
+
+		int age = Convert.ToInt32(Console.ReadLine ());
+
+		Console.WriteLine (age+10);
+			
+		}
+	}
---
+++ PrintSequence/PrintSequence/PrintSequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Sequence
+{
+	public static void Main ()
+	{
+		//2, -3, 4, -5, 6, -7, ...
+
+		for (int i = 2; i < 12; i++) 
+		
+		{
+			if (i % 2 == 0) 
+			
+			{
+				Console.WriteLine (i);
+			} 
+
+			else 
+			
+			{
+
+				Console.WriteLine (-i);
+			}
+		}
+	}
+}
