584228382d6dcd6fe2beebe014bd0a1820176fc2
Dessyslava Wasseva <desssssy@gmail.com>
2015-07-27 18:49:11 +0300

    First Last name
    Print Fist and Last Name HomeWork added

---
+++ PrintFirstLastNames/PrintFirstLastNames/PrintFirstLastNames.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class PrintFirstLast
+{
+	public static void Main ()
+	{
+		Console.WriteLine ("Dessyslava");
+		Console.WriteLine ("Wasseva");
+		Console.ReadLine ();
+	}
+}
---
+++ PrintName/PrintName/PrintName.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class PrintMyName
+{
+	public static void Main ()
+	{
+		Console.WriteLine ("Dessyslava Wasseva");
+		Console.ReadLine ();
+	}
+}
