bd2d9d839c6b15e901027e7dc1cbb0ba05bd7cf1
Dessyslava Wasseva <desssssy@gmail.com>
2015-08-01 14:54:01 +0300

    added problems up to 10
    Added problems up to 10

---
+++ PrimitiveDataTypesAndVariables/EmployeeData/EmployeeData.cs
@@ -0,0 +1,49 @@
+﻿//Employee Data
+
+//A marketing company wants to keep record of its employees. Each record would have the following characteristics:
+
+//First name
+//Last name
+//Age (0...100)
+//Gender (m or f)
+//Personal ID number (e.g. 8306112507)
+//Unique employee number (27560000…27569999)
+//Declare the variables needed to keep the information for a single employee using appropriate primitive data types. 
+//Use descriptive names. Print the data at the console.//
+
+using System;
+
+class EmployeeData
+{
+	public static void Main ()
+	{
+		string firstName;
+		string lastName;
+		byte age;
+		bool isFeemail;
+		long PersonalIDNumber;
+		int EmployeeIDNumber;
+
+		firstName = "Dessyslava";
+		lastName = "Wasseva";
+		age = 33;
+		isFeemail = true;
+		PersonalIDNumber = 8306112507;
+		EmployeeIDNumber = 27560000;
+
+		Console.WriteLine ("First Name: {0} \nLast Name: {1}\nAge: {2}",firstName,lastName,age);
+
+		if (isFeemail == true) {
+			Console.WriteLine ("Gender: f");
+		} 
+
+		else {
+
+			Console.WriteLine ("Gender: m");
+
+		}
+
+		Console.WriteLine ("Personal ID Number: {0}\nEmployee ID Number: {1}",PersonalIDNumber,EmployeeIDNumber);
+	
+	}
+}
---
+++ PrimitiveDataTypesAndVariables/IsoscelesTriangle/Isosceles.cs
@@ -0,0 +1,26 @@
+﻿//Isosceles Triangle
+
+//Write a program that prints an isosceles triangle of 9 copyright symbols ©, something like this:
+//©
+
+//© ©
+
+//©   ©
+
+//© © © ©
+//Note: The © symbol may be displayed incorrectly at the console so you may need to change the console character encoding to UTF-8 and assign a Unicode-friendly font in the console.
+
+//Note: Under old versions of Windows the © symbol may still be displayed incorrectly, regardless of how much effort you put to fix it.
+
+
+using System;
+
+class IsoscelesTriangle
+{
+	public static void Main ()
+	{
+		char isoscale = '\u00A9';
+
+		Console.WriteLine ("   {0}   \n  {0} {0} \n {0}   {0} \n{0} {0} {0} {0}",isoscale);
+	}
+}
