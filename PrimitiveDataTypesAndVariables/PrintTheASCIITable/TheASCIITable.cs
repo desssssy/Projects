c03bc8f6045f330a3dc630fdb380941ef832a0f0
Dessyslava Wasseva <desssssy@gmail.com>
2015-08-01 16:40:37 +0300

    Problem 14 added
    Solution to Problem 14 added

---
+++ PrimitiveDataTypesAndVariables/PrintTheASCIITable/TheASCIITable.cs
@@ -0,0 +1,29 @@
+﻿//Print the ASCII Table
+
+//Find online more information about ASCII (American Standard Code for Information Interchange) 
+//and write a program that prints the entire ASCII table of characters on the console (characters from 0 to 255).
+//Note: Some characters have a special purpose and will not be displayed as expected. You may skip them or display 
+//them differently.
+
+//Note: You may need to use for-loops (learn in Internet how).
+
+
+using System;
+
+class PrintTheASCIITable
+{
+	public static void Main ()
+	{
+
+		for (int i = 0; i <= 255; i++) {
+
+			if (Char.IsDigit ((char)i) || Char.IsNumber ((char)i) || Char.IsSymbol ((char)i) || Char.IsLetter ((char)i))
+			
+			{
+				Console.WriteLine ((char)i);
+			}
+			
+		}
+			
+	}
+}
