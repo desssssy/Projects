a52303d8e85d7390daadcd2583fef7b0a50582bc
Dessyslava Wasseva <desssssy@gmail.com>
2015-08-02 20:04:17 +0300

    Problem 1
    Added Problem 1 Even Odd check in 2 ways

---
+++ OperatorsAndExpressions/OddOrEvenIntegers/OddOrEven.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class OddOrEvenIntegers
+{
+	public static void Main ()
+	{
+
+		int input = int.Parse (Console.ReadLine ());
+
+		int check = input & 1;
+
+		if (check == 1) 
+		
+		{
+			Console.WriteLine ("Odd");
+
+		}
+
+		else 
+		
+		{
+			Console.WriteLine ("Even");
+
+		}
+
+
+		int input2 = int.Parse (Console.ReadLine ());
+
+		if (input2 % 2 == 0) 
+		
+		{
+			Console.WriteLine ("Even");
+		} 
+
+		else 
+		
+		{
+
+			Console.WriteLine ("Odd");
+		}
+
+	}
+}
