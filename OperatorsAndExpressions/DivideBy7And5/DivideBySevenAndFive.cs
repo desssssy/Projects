a355adb104a100e72ea7d3cb3ca23a0131359a18
Dessyslava Wasseva <desssssy@gmail.com>
2015-08-02 20:28:35 +0300

    Problem 3
    Problem 3 Divided by 5 and 7 at the same time added

---
+++ OperatorsAndExpressions/DivideBy7And5/DivideBySevenAndFive.cs
@@ -0,0 +1,23 @@
+﻿//Problem 3. Divide by 7 and 5
+
+//Write a Boolean expression that checks for given integer if it can be divided (without remainder) by 7 and 5 at the same time.
+	
+
+using System;
+
+
+class DivideBy7And5
+{
+	public static void Main ()
+	{
+
+		int input = int.Parse (Console.ReadLine ());
+
+		bool canBeDividedBy7And5 = ((input % 7) == 0) && ((input % 5 == 0));
+
+
+		Console.WriteLine (canBeDividedBy7And5);
+
+			
+	}
+}
