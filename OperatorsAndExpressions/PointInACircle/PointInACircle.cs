d449cf06e62e10fb450b104a8c4c935f54e186de
Dessyslava Wasseva <desssssy@gmail.com>
2015-08-02 23:49:05 +0300

    Problem 7
    Problem 7 - Point in a Circle added

---
+++ OperatorsAndExpressions/PointInACircle/PointInACircle.cs
@@ -0,0 +1,28 @@
+﻿//Problem 7. Point in a Circle
+
+//Write an expression that checks if given point (x, y) is inside a circle K({0, 0}, 2).
+
+using System;
+
+class PointInACircle
+{
+	public static void Main ()
+	{
+
+		double xMax = 2;
+		double xMin = -2;
+		double yMax = 2;
+		double yMin = -2;
+
+		Console.WriteLine ("Please enter x:");
+		double x = double.Parse (Console.ReadLine ());
+
+		Console.WriteLine ("Plese enter y:");
+		double y = double.Parse (Console.ReadLine ());
+
+		bool dotInCircle = ((x <= xMax && x >= xMin) && (y <= yMax && y >= yMin));
+
+		Console.WriteLine (dotInCircle);
+		
+	}
+}
